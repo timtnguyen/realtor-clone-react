60931d22d8c63c12d3c4f25d76acfc98bd57e02e
thinh nguyen <thinhnguyen@thinhs-MacBook-Air.local>
2023-10-07 22:15:19 -0700

    create the create listing page UI

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import CreateListing from "./pages/CreateListing";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
             <Route path="/profile" element={<Profile />} />
           </Route>
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
       </Router>
       <ToastContainer
